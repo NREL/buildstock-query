14cd4b05dba020306960912124245f721291a8ba
Andrew Parker <Andrew.Parker@nrel.gov>
2023-11-07 10:59:38 -0700

    Removes colorama dependency

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
         "SQLAlchemy == 1.4.46",
         "sqlalchemy2-stubs",
         "pandas-stubs",
-        "colorama >= 0.4.5",
         "inquirerpy >= 0.3.4",
         "types-PyYAML >= 6.0.12.2",
         "pydantic<2",
